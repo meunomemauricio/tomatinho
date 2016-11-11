4cd332f91aaa1ee3735fb1610dda66680cfa1991
Mauricio Freitas <contato@mauriciofreitas.eng.br>
2016-11-11 17:23:52 -0200

    Update to Python3 and GTK3
    Also using the AppIndicator instead of StatusIcon, which makes it
    possible to run on Ubuntu with Unity.
    
    Also using Notify from the GTK3, instead of using DBus

--- src/tomatinho.py
+++ src/tomatinho.py
@@ -1,18 +1,20 @@
-#!/usr/bin/python
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import datetime
-import dbus
-import gobject
-import gtk
 import os.path
-import re
+import signal
 import sqlite3
-import sys
-import time
 
-sqlite3.register_adapter(bool, int)
-sqlite3.register_converter('BOOLEAN', lambda v: bool(int(v)))
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('AppIndicator3', '0.1')
+gi.require_version('Notify', '0.7')
+
+from gi.repository import Gtk  # noqa: E402
+from gi.repository import AppIndicator3  # noqa: E402
+from gi.repository import Notify  # noqa: E402
+from gi.repository import GObject  # noqa: E402
 
 CUR_DIR = os.path.dirname(os.path.realpath(__file__))
 HOME_DIR = os.path.expanduser('~')
@@ -24,13 +26,13 @@ def start_database():
         os.makedirs(TOMATINHO_DIR)
 
     conn = sqlite3.connect(os.path.join(TOMATINHO_DIR, 'tomatinho.db'))
-    c = conn.cursor()
+    cursor = conn.cursor()
 
     # Create Statistics Table
     sql_cmd = ('CREATE TABLE statistics'
-               '(oper TEXT, completed BOOLEAN, created TEXT)')
+               '(oper TEXT, completed BOOLEAN, datetime TEXT)')
     try:
-        c.execute(sql_cmd)
+        cursor.execute(sql_cmd)
         conn.commit()
         conn.close()
 
@@ -39,8 +41,13 @@ def start_database():
         pass
 
 
-class TomatinhoTimer(object):
+class Tomatinho(object):
     """Pomodoro Timer Application"""
+
+    # GTK Variables
+    APPINDICATOR_ID = "tomatinho"
+
+    # Icons
     ICON_IDLE = os.path.join(CUR_DIR, 'icons/tomate-idle.png')
     ICON_POMO = os.path.join(CUR_DIR, 'icons/tomate-pomo.png')
     ICON_REST_S = os.path.join(CUR_DIR, 'icons/tomate-rest-s.png')
@@ -52,204 +59,149 @@ class TomatinhoTimer(object):
     REST_S_STATE = 'Pausa Curta'
     REST_L_STATE = 'Pausa Longa'
 
-    # DBus for Desktop Notifications
-    BUS_NAME = 'org.freedesktop.Notifications'
-    OBJECT_PATH = '/org/freedesktop/Notifications'
-    INTERFACE_NAME = 'org.freedesktop.Notifications'
-
-    NOTIFICATION_TIME = 5000
-
     def __init__(self):
-        self.tray = gtk.StatusIcon()
-        self.tray.set_from_file(self.ICON_IDLE)
-        self.tray.set_tooltip('Status: Parado')
+        self.indicator = AppIndicator3.Indicator.new(
+            self.APPINDICATOR_ID,
+            self.ICON_IDLE,
+            AppIndicator3.IndicatorCategory.APPLICATION_STATUS,
+        )
+        self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
+
+        self.menu = None
+        self.build_menu()
 
         self.state = self.IDLE_STATE
-
-        self.tray.connect('popup-menu', self.on_right_click)
-        self.tray.connect('activate', self.on_left_click)
-
         self.timer_running = False
         self.time_left = 0
 
-        self.statistics_db = sqlite3.connect(os.path.join(TOMATINHO_DIR, 'tomatinho.db'))
+        self.countdown_timer_id = None
 
-    def notify(self, summary):
-        session_bus = dbus.SessionBus()
-        obj = session_bus.get_object(self.BUS_NAME, self.OBJECT_PATH)
+        self.statistics_db = sqlite3.connect(
+            os.path.join(TOMATINHO_DIR, 'tomatinho.db'))
 
-        interface = dbus.Interface(obj, self.INTERFACE_NAME)
+    def build_menu(self):
+        self.menu = Gtk.Menu()
+        self.add_new_menu_item('Tomatar', self.start_pomodoro)
+        self.add_new_menu_item('Pausa Curta', self.start_short_rest)
+        self.add_new_menu_item('Pausa Longa', self.start_long_rest)
+        self.add_new_menu_item('Parar', self.stop_timer)
+        self.menu.append(Gtk.SeparatorMenuItem())
+        self.add_new_menu_item('Sobre', self.about_dialog)
+        self.menu.append(Gtk.SeparatorMenuItem())
+        self.add_new_menu_item('Sair', self.quit)
+        self.menu.show_all()
 
-        interface.Notify('', 0, '', summary, '', [], [], self.NOTIFICATION_TIME)
+        self.indicator.set_menu(self.menu)
+        Notify.init(self.APPINDICATOR_ID)
 
-    def on_left_click(self, icon):
-        self.tray.set_blinking(False)
-
-    def on_right_click(self, icon, event_button, event_time):
-        self.tray.set_blinking(False)
-
-        menu = gtk.Menu()
-
-        # Start Pomodoro
-        menu_item = gtk.MenuItem('Tomatar')
-        menu_item.show()
-        menu.append(menu_item)
-        menu_item.connect('activate', self.start_pomodoro)
-
-        menu_item = gtk.MenuItem('Pausa Curta')
-        menu_item.show()
-        menu.append(menu_item)
-        menu_item.connect('activate', self.start_short_rest)
-
-        menu_item = gtk.MenuItem('Pausa Longa')
-        menu_item.show()
-        menu.append(menu_item)
-        menu_item.connect('activate', self.start_long_rest)
-
-        menu_item = gtk.MenuItem('Parar')
-        menu_item.show()
-        menu.append(menu_item)
-        menu_item.connect('activate', self.stop_timer)
-
-        # Separator
-        separator = gtk.SeparatorMenuItem()
-        separator.show()
-        menu.append(separator)
-
-        # About dialog
-        about = gtk.MenuItem('Sobre')
-        about.show()
-        menu.append(about)
-        about.connect('activate', self.show_about_dialog)
-
-        separator = gtk.SeparatorMenuItem()
-        separator.show()
-        menu.append(separator)
-
-        # Add quit item
-        quit = gtk.MenuItem('Sair')
-        quit.show()
-        menu.append(quit)
-        quit.connect('activate', self.quit_application)
-
-        menu.popup(None, None, gtk.status_icon_position_menu,
-                   event_button, event_time, self.tray)
+    def add_new_menu_item(self, text, action):
+        menu_item = Gtk.MenuItem(text)
+        menu_item.connect('activate', action)
+        self.menu.append(menu_item)
 
     def start_countdown_timer(self, start_time):
         if self.timer_running:
-            gobject.source_remove(self.countdown_timer_id)
+            GObject.source_remove(self.countdown_timer_id)
 
         # 1s Timer
-        self.countdown_timer_id = gobject.timeout_add(1000, self.countdown)
+        self.countdown_timer_id = GObject.timeout_add(1000, self.countdown)
 
         # Time left in seconds
         self.time_left = start_time
-        self.update_tooltip()
-
         self.timer_running = True
 
     def stop_countdown_timer(self):
         if self.timer_running:
-            gobject.source_remove(self.countdown_timer_id)
+            GObject.source_remove(self.countdown_timer_id)
             self.timer_running = False
-
-        self.update_tooltip()
 
     def countdown(self):
         self.time_left -= 1
 
         if self.time_left == 0:
             self.record_statistic(self.state, completed=True)
-            self.tray.set_blinking(True)
-            self.notify('Contador Parado.')
-            self.tray.set_from_file(self.ICON_IDLE)
             self.state = self.IDLE_STATE
             self.stop_countdown_timer()
+            self.indicator.set_icon(self.ICON_IDLE)
+            self.notify('Contador Parado', self.ICON_IDLE)
             return False
-
-        self.update_tooltip()
 
         return True
 
-    def update_tooltip(self):
-        if self.state != self.IDLE_STATE:
-            time_left = datetime.timedelta(seconds=self.time_left)
-            tooltip = 'Status: {0} - Time Left: {1}'.format(self.state, time_left)
-        else:
-            tooltip = 'Status: {0}'.format(self.state)
-
-        self.tray.set_tooltip(tooltip)
-
-    def stop_timer(self, widge=None):
-        self.notify('Contador Parado.')
-
+    def start_pomodoro(self, source):
         if self.state != self.IDLE_STATE:
             self.record_statistic(self.state, completed=False)
 
-        self.tray.set_from_file(self.ICON_IDLE)
-        self.state = self.IDLE_STATE
-
-        self.stop_countdown_timer()
-
-    def start_pomodoro(self, widget=None):
-        if self.state != self.IDLE_STATE:
-            self.record_statistic(self.state, completed=False)
-
-        self.notify('Tomatando (25m)')
-
-        self.tray.set_from_file(self.ICON_POMO)
         self.state = self.POMO_STATE
         self.start_countdown_timer(25 * 60)
+        self.indicator.set_icon(self.ICON_POMO)
+        self.notify('Tomatando (25m)', self.ICON_POMO)
 
-    def start_short_rest(self, widget=None):
+    def start_short_rest(self, source):
         if self.state != self.IDLE_STATE:
             self.record_statistic(self.state, completed=False)
 
-        self.notify('Pausa Curta (3m)')
-
-        self.tray.set_from_file(self.ICON_REST_S)
         self.state = self.REST_S_STATE
         self.start_countdown_timer(3 * 60)
+        self.indicator.set_icon(self.ICON_REST_S)
+        self.notify('Pausa Curta (3m)', self.ICON_REST_S)
 
-    def start_long_rest(self, widget=None):
+    def start_long_rest(self, source):
         if self.state != self.IDLE_STATE:
             self.record_statistic(self.state, completed=False)
 
-        self.notify('Pausa Longa (15m)')
+        self.state = self.REST_S_STATE
+        self.start_countdown_timer(3 * 60)
+        self.indicator.set_icon(self.ICON_REST_L)
+        self.notify('Pausa Longa (15m)', self.ICON_REST_L)
 
-        self.tray.set_from_file(self.ICON_REST_L)
-        self.state = self.REST_L_STATE
-        self.start_countdown_timer(15 * 60)
+    def stop_timer(self, source):
+        if self.state != self.IDLE_STATE:
+            self.record_statistic(self.state, completed=False)
+
+        self.state = self.IDLE_STATE
+        self.stop_countdown_timer()
+        self.indicator.set_icon(self.ICON_IDLE)
+        self.notify('Contador Parado', self.ICON_IDLE)
+
+    def notify(self, message, icon):
+        Notify.Notification.new('Tomatinho', message, icon).show()
 
     def record_statistic(self, operation, completed):
-        c = self.statistics_db.cursor()
-
         current_datetime = datetime.datetime.now()
-        c.execute('INSERT INTO statistics VALUES (?, ?, ?)',
-                  (operation, completed, current_datetime))
-
+        self.statistics_db.cursor().execute(
+            'INSERT INTO statistics VALUES (?, ?, ?)',
+            (operation, completed, current_datetime)
+        )
         self.statistics_db.commit()
 
-    def show_about_dialog(self, widget):
-        about_dialog = gtk.AboutDialog()
-        about_dialog.set_destroy_with_parent (True)
+    def about_dialog(self, source):
+        about_dialog = Gtk.AboutDialog()
+        about_dialog.set_destroy_with_parent(True)
         about_dialog.set_icon_name('Tomatinho Timer')
         about_dialog.set_name('Tomatinho Timer')
         about_dialog.set_version('0.1')
-        about_dialog.set_copyright('(C) 2015 Mauricio Freitas')
-        about_dialog.set_comments('Simples aplicativo temporizador para Técnica Pomodoro')
+        about_dialog.set_copyright('(C) 2016 Mauricio Freitas')
+        about_dialog.set_comments('Simples aplicativo temporizador para '
+                                  'Técnica Pomodoro')
         about_dialog.set_authors(['Mauricio Freitas'])
         about_dialog.run()
         about_dialog.destroy()
 
-    def quit_application(self, widget):
-        self.stop_countdown_timer()
-        gtk.main_quit()
+    def quit(self, source):
+        if self.state != self.IDLE_STATE:
+            self.record_statistic(self.state, completed=False)
+
+        Gtk.main_quit()
 
 
-if __name__ == '__main__':
-
+def main():
+    # Enable quiting the app with ^C
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
     start_database()
+    Tomatinho()
+    Gtk.main()
 
-    tomatinho = TomatinhoTimer()
-    gtk.main()
+
+if __name__ == "__main__":
+    main()
